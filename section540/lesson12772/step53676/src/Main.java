08743d2852097849fc66116907f7317d8afd38fb
daniiliv <dania-spb@mail.ru>
2019-02-27 14:12:32 +0200

    27.02. Знакомство с исключениями.
    1. Метод sqrt, выбрасывающий исключение при отрицательном параметре.
    2. Метод, позволяющий другим методам узнать, откуда их вызвали.

--- section540/lesson12772/step53676/src/Main.java
+++ section540/lesson12772/step53676/src/Main.java
@@ -2,6 +2,7 @@ class Main {
     //Stepik code: start
 
     /**
+     *
      * Реализуйте метод, позволяющий другим методам узнать, откуда их вызвали.
      *
      * Метод getCallerClassAndMethodName() должен возвращать имя класса и метода, откуда вызван метод, вызвавший данный утилитный метод. Или null (нулевую ссылку, а не строку "null"), если метод, вызвавший getCallerClassAndMethodName() является точкой входа в программу, т.е. его никто не вызывал.
@@ -11,14 +12,21 @@ class Main {
      */
     public static String getCallerClassAndMethodName() {
         Throwable thr = new Throwable("error");
+
+        /**
+         * В стеке всегда метод, вызвавший StackTrace, будет на позиции [2].
+         *
+         * 0 - getStackTrace;
+         * 1 - имя метода (своё имя)
+         * 2 - имя метода, кто вызвал [1]
+         * 3 - имя метода, кто вызвал [2]
+         * 4 - и т.д.
+         */
         StackTraceElement[] elements = thr.getStackTrace();
         if (elements.length >= 3) {
             return elements[2].getClassName() + "#" + elements[2].getMethodName();
         }
-
-
-
-        return null; // your implementation here
+        return null;
     }
 //Stepik code: end
 }
